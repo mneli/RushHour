9bc0b18def4578462600d2fdcf52f14b847e839f
g42861 <42861@heb.be>
2016-03-20 17:58:10 +0100

    Enumeration Direction + javadoc Direction

--- src/g42861/rushhour/model/Direction.java
+++ src/g42861/rushhour/model/Direction.java
@@ -1,10 +1,18 @@
 package g42861.rushhour.model;
 
 /**
+ * Direction can take 4 values:
+ * <li>LEFT for movement to the left</li>
+ * <li>RIGHT for movement to the right</li>
+ * <li>UP for movement to up</li>
+ * <li>DOWN for movement to down</li>
  *
  * @author G42861
  * @group B231
  */
 public enum Direction {
-
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN;
 }
