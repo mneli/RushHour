43bdbe4d3188bcc31656acab13d0b67118411e1f
g42861 <42861@heb.be>
2016-03-21 10:52:48 +0100

    enumeration Orientation + javadoc Orientation

---
+++ src/g42861/rushhour/model/Orientation.java
@@ -0,0 +1,14 @@
+package g42861.rushhour.model;
+
+/**
+ * Orientation can take 2 values:
+ * <li>HORIZONTAL for horizontal orientation</li>
+ * <li>VERTICAL for vertical orientation</li>
+ *
+ * @author G42861
+ * @group B231
+ */
+public enum Orientation {
+    HORIZONTAL,
+    VERTICAL;
+}
