8fc6029d9a8cc3be58514087cd5e5f972b22ec0a
g42861 <42861@heb.be>
2016-03-22 19:05:31 +0100

    V2: javadoc class Board part 1

--- src/g42861/rushhour/model/Board.java
+++ src/g42861/rushhour/model/Board.java
@@ -12,6 +12,13 @@ public class Board {
     private Car[][] grid;
     private Position exit;
 
+    /**
+     * Construct an instance of Board.
+     *
+     * @param height the number of rows of the board
+     * @param width the number of columns of the board
+     * @param exit the position of the exit
+     */
     public Board(int height, int width, Position exit) {
         if (height <= 0)
             throw new IllegalArgumentException("Invalid height");
@@ -25,26 +32,65 @@ public class Board {
         this.exit = exit;
     }
 
+    /**
+     * Construct an instance of Board with default values. The default values
+     * are :
+     * <li>height : 6</li>
+     * <li>width : 6</li>
+     * <li>exit at Position (2,5)</li>
+     */
     public Board() {
         this(6, 6, new Position(2, 5));
     }
 
+    /**
+     * Get the exit of an instance of Board.
+     *
+     * @return the position of the exit
+     */
     public Position getExit() {
         return this.exit;
     }
 
+    /**
+     * Get the height(number of rows) of a board.
+     *
+     * @return the height
+     */
     public int getHeight() {
         return this.grid.length;
     }
 
+    /**
+     * Get the width(number of columns) of a board.
+     *
+     * @return the width
+     */
     public int getWidth() {
         return this.grid[0].length;
     }
 
+    /**
+     * Get the car at the position received in parameter.
+     *
+     * @param pos the position to check
+     * @return a car or null if the position received in parameter doesn't
+     * contains a car
+     */
     public Car getCarAt(Position pos) {
         return this.grid[pos.getRow()][pos.getColumn()];
     }
 
+    /**
+     * Verify if a car can be placed on board.
+     * <li>Check if every position that the car received in parameter want to
+     * occupy on board doesn't contain already an another car</li>
+     * <li>Check if every position that the car received in parameter want to
+     * occupy on board are on board and not outside</li>
+     *
+     * @param car the instance of Car
+     * @return true if the car can be placed
+     */
     public boolean canPut(Car car) {
         List<Position> listPos = car.getPositions();
         int counter = 0;
@@ -57,6 +103,11 @@ public class Board {
         return counter == listPos.size();
     }
 
+    /**
+     * Place a car on board.
+     *
+     * @param car the instance of Car to place on board
+     */
     public void put(Car car) {
         List<Position> listPos = car.getPositions();
         for (Position pos : listPos) {
