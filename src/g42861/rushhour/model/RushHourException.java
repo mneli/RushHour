64a09380921745096b98ccacb9685d29f9b6badb
g42861 <42861@heb.be>
2016-03-23 01:14:30 +0100

    V2: class RushHourException

---
+++ src/g42861/rushhour/model/RushHourException.java
@@ -0,0 +1,30 @@
+package g42861.rushhour.model;
+
+/**
+ *
+ * @author G42861
+ * @group B231
+ */
+public class RushHourException extends Exception {
+
+    /**
+     * Creates a new instance of <code>RushHourException</code> without detail
+     * message.
+     */
+    public RushHourException() {
+    }
+
+    /**
+     * Constructs an instance of <code>RushHourException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public RushHourException(String msg) {
+        super(msg);
+    }
+}
+/*
+    @issue
+    constructeur vide à supprimer?
+*/
