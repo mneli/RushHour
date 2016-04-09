2c854f605f5cb6a72ee9ff6d1943b24599f803ec
g42861 <42861@heb.be>
2016-04-09 21:31:26 +0200

    V2 : Game ready

--- src/g42861/rushhour/RushHour.java
+++ src/g42861/rushhour/RushHour.java
@@ -1,5 +1,13 @@
-
 package g42861.rushhour;
+
+import g42861.rushhour.model.Car;
+import g42861.rushhour.model.Orientation;
+import g42861.rushhour.model.Position;
+import g42861.rushhour.model.RushHourException;
+import g42861.rushhour.model.RushHourGame;
+import g42861.rushhour.view.RushHourView;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -10,9 +18,37 @@ public class RushHour {
 
     /**
      * @param args the command line arguments
+     * @throws g42861.rushhour.model.RushHourException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws RushHourException {
+
+        Position exit = new Position(2, 5);
+
+        Car car1 = new Car('1', 3, Orientation.VERTICAL, new Position(0, 0));
+        Car car2 = new Car('2', 2, Orientation.VERTICAL, new Position(4, 0));
+        Car car3 = new Car('3', 2, Orientation.HORIZONTAL, new Position(0, 1));
+        Car car4 = new Car('4', 2, Orientation.HORIZONTAL, new Position(4, 1));
+        Car car5 = new Car('5', 3, Orientation.VERTICAL, new Position(1, 3));
+        Car car6 = new Car('6', 3, Orientation.HORIZONTAL, new Position(5, 2));
+        Car car7 = new Car('7', 3, Orientation.VERTICAL, new Position(3, 5));
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+        cars.add(car6);
+        cars.add(car7);
+
+        Car redCar = new Car('R', 2, Orientation.HORIZONTAL, new Position(2, 1));
+
+        RushHourGame game = new RushHourGame(6, 6, exit, cars, redCar);
+
+        RushHourView view = new RushHourView(game);
+
+        view.play();
+
     }
 
 }
--- src/g42861/rushhour/view/RushHourView.java
+++ src/g42861/rushhour/view/RushHourView.java
@@ -1,7 +1,9 @@
 package g42861.rushhour.view;
 
+import g42861.rushhour.model.Direction;
 import g42861.rushhour.model.RushHourException;
 import g42861.rushhour.model.RushHourGame;
+import java.util.Scanner;
 
 /**
  *
@@ -9,21 +11,72 @@ import g42861.rushhour.model.RushHourGame;
  * @group B231
  */
 public class RushHourView {
-
+    
     private RushHourGame game;
-
+    
     public RushHourView(RushHourGame game) {
         this.game = game;
     }
-
+    
     public void play() throws RushHourException {
-
+        System.out.println("Game start");
+        Display.displayBoard(this.game.getBoard());
         while (!game.isOver()) {
             //demander id
+            char id = scanChar("Enter car id : ");
             //demander direction
+            Direction direction = scanDirection("Enter direction");
             //deplacer car
+            this.game.move(id, direction);
             //afficher plateau
+            Display.displayBoard(this.game.getBoard());
         }
+        
+    }
 
+    /**
+     * Lit des caractères au clavier
+     *
+     * @return le premier caractère lu
+     */
+    public static char scanChar(String msg) {
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNext()) {
+            System.out.print(msg);
+            scanner.next();
+        }
+        return scanner.next().charAt(0);
+    }
+
+    /**
+     * Lit des caractères au clavier
+     *
+     * @return le premier caractère lu
+     */
+    public Direction scanDirection(String msg) {
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNext()
+                && !scanner.next().toUpperCase().equals("UP")
+                && !scanner.next().toUpperCase().equals("DOWN")
+                && !scanner.next().toUpperCase().equals("LEFT")
+                && !scanner.next().toUpperCase().equals("RIGHT")) {
+            System.out.print(msg);
+            scanner.next();
+        }
+        Direction direction = null;
+        switch (scanner.next().toUpperCase()) {
+            case "UP":
+                direction = Direction.UP;
+                break;
+            case "DOWN":
+                direction = Direction.DOWN;
+                break;
+            case "LEFT":
+                direction = Direction.LEFT;
+                break;
+            case "RIGHT":
+                direction = Direction.RIGHT;
+        }
+        return direction;
     }
 }
