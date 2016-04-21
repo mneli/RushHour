772fd530d40b1c07455bac879807b840b013f769
G42861 <G42861@L303P15.pdg.esi.heb.site>
2016-04-21 15:41:52 +0200

    evaluation : 4/Race, deuxième partie

--- src/g42861/evaluation/Race.java
+++ src/g42861/evaluation/Race.java
@@ -1,6 +1,7 @@
 package g42861.evaluation;
 
 import g42861.rushhour.model.Direction;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -50,5 +51,56 @@ public class Race {
             }
         }
         return distanceParcourue;
+    }
+
+    /**
+     * Determine le vainqueur d'une course.
+     *
+     * @param n le nombre de mouvement que chaque escargot doit faire
+     * @return l'escargot qui a le plus grand score
+     */
+    public Snail run(int n) {
+        Snail vainqueur = null;
+        int score;
+        int scoreMax = Integer.MIN_VALUE;
+
+        for (Snail snail : snails) {
+            score = distance(snail.getMoves(n));
+            if (score > scoreMax) {
+                scoreMax = score;
+                vainqueur = snail;
+            }
+        }
+
+        return vainqueur;
+    }
+
+    /**
+     * Instancie une course, affiche le nom des participant et le vainqueur en
+     * fin de course.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        List<Snail> snails = new ArrayList<>();
+        snails.add(new Snail("Turbo"));
+        snails.add(new Snail("Taco-Man"));
+        snails.add(new Snail("Smoove move"));
+        snails.add(new Snail("Guy la gagne"));
+
+        Race course = null;
+        try {
+            course = new Race(snails);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("la course commence avec : ");
+        for (Snail snail : course.snails) {
+            System.out.print(snail.getName() + ", ");
+        }
+        int nbDeplacement = (int) (Math.random() * 20) + 1;
+        System.out.println("\nVainqueur : " + course.run(nbDeplacement).getName());
     }
 }
